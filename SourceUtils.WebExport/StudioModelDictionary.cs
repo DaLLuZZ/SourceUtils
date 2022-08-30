8efa30d40a8e91515a57f5c0b122df24b62b26a4
DaLLuZZ <79600653+DaLLuZZ@users.noreply.github.com>
2022-08-30 08:17:42 +0300

    Update StudioModelDictionary.cs

--- SourceUtils.WebExport/StudioModelDictionary.cs
+++ SourceUtils.WebExport/StudioModelDictionary.cs
@@ -46,11 +46,10 @@ namespace SourceUtils.WebExport
 
             foreach ( var item in items )
             {
-                File.AppendAllText("models.txt", $"item.Path: {item.Path}" + Environment.NewLine);
+                File.AppendAllText("models.txt", $"[{GetResourceIndex( item.Path )}] [{_vertexCounts.Count}] item.Path: {item.Path}" + Environment.NewLine);
                 yield return item.Path;
 
                 var index = GetResourceIndex( item.Path );
-                File.AppendAllText("models.txt", $"index: {index}\n" + Environment.NewLine);
                 if ( index == _vertexCounts.Count )
                 {
                     _vertexCounts.Add( item.VertexCount );
